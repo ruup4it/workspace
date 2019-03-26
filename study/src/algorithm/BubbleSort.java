3b9c580f6a35c953c55a8a3d95459a7065c16025
ruup4it <ruup4it@naver.com>
2019-03-26 11:33:56 +0900

    commit_stickMemo

--- study/src/algorithm/BubbleSort.java
+++ study/src/algorithm/BubbleSort.java
@@ -37,24 +37,25 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String input = scan.nextLine();
-		String[] strArr = input.split("");
-
+		String[] strArr = input.split(" ");
 		int[] arr = new int[strArr.length];
+
 		for (int i = 0; i < strArr.length; i++) {
 			arr[i] = Integer.parseInt(strArr[i]);
 		}
-
-		for (int i = 0; i < arr.length - 1; i++) {
-			if (arr[i] > arr[i + 1]) {
-				int temp = arr[i];
-				arr[i] = arr[i + 1];
-				arr[i + 1] = temp;
-				for (int item : arr) {
-					System.out.print(item);
+		int temp = 0;
+		for (int i = 0; i < arr.length-1; i++) {
+			for (int j = 0; j < arr.length - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					for (int item : arr) {
+						System.out.print(item+ " ");
+					}
+					System.out.println();
 				}
-				System.out.println();
 			}
-
 		}
 
 		scan.close();
